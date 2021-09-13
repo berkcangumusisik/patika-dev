08b3335309e480cd6e4e7df6bf4c9db7a42db340
Berkcan Gümüşışık <gumusisikberkcan@gmail.com>
2021-09-13 17:14:11 +0300

    C# lesson 1 added

---
+++ C#/Pratikler/01 -ConsoleProgramlama/01 -ConsoleProgramlama/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _01__ConsoleProgramlama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("İsminizi Giriniz:");
+            string isim = Console.ReadLine();
+            Console.WriteLine("Soyadınızı Giriniz:");
+            string soyad = Console.ReadLine();
+            Console.WriteLine("Merhaba " + isim + " " + soyad);
+        }
+    }
+}
+
+
+// Console.WriteLine() ekrana yazdırmayı sağlar
