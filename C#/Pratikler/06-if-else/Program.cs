b70ddd0e8ccd26b802570a3fc93777fbd1d181cf
Berkcan Gümüşışık <gumusisikberkcan@gmail.com>
2022-02-03 17:15:09 +0300

    If Else Yapısı Eklendi

---
+++ C#/Pratikler/06-if-else/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _06_if_else
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+            if (time >= 6 && time <= 11)
+            {
+                Console.WriteLine("Günaydın");
+
+            }
+            else if (time < 18)
+            {
+                Console.WriteLine("İyi Günler");
+            }
+            else
+            {
+                Console.WriteLine("İyi Geceler");
+            }
+            string a = time >= 6 && time <= 11 ? "Günaydın!" : time <= 18 ? "İyi günler!" : "İyi geceler!";  
+
+        }
+    }
+}
