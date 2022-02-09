9c37fc68c1dae19b70f7e98addcff7944ed7c16e
Berkcan Gümüşışık <gumusisikberkcan@gmail.com>
2022-02-09 10:14:56 +0300

    3.Algoritma Eklendi.

---
+++ C#/ödevler/Homework1/algoritmaSorusu3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace algoritmaSorusu3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen Bir Sayı Giriniz : ");
+            int n = int.Parse(Console.ReadLine());
+            string[] kelimeDizisi = new string[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Lütfen {0}. Kelimeyi Giriniz.",i+1);
+                kelimeDizisi[i] = Convert.ToString(Console.ReadLine());
+            }
+
+            foreach (var kelime in kelimeDizisi)
+            {
+                Console.WriteLine(kelime);
+            }
+            Console.WriteLine("******************************");
+            
+            Array.Reverse(kelimeDizisi);
+            foreach (var kelime in kelimeDizisi)
+            {
+                Console.WriteLine(kelime);
+            }
+        }
+    }
+}
+
+/*
+Bir konsol uygulamasında kullanıcıdan pozitif bir sayı girmesini isteyin (n). 
+Sonrasında kullanıcıdan n adet kelime girmesi isteyin. 
+Kullanıcının girişini yaptığı kelimeleri sondan başa doğru console'a yazdırın.
+*/
