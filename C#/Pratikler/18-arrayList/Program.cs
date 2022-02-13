3e6c242c451a3726daad810f7901dd668e2f86d8
Berkcan Gümüşışık <gumusisikberkcan@gmail.com>
2022-02-13 11:48:17 +0300

    Array List Addes

---
+++ C#/Pratikler/18-arrayList/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace _18_arrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList liste = new ArrayList();
+            liste.Add("Berkcan");
+            liste.Add("Şevval");
+            liste.Add("Nurana");
+            liste.Add("Mete Serkan");
+            liste.Add(2);
+            liste.Add(true);
+
+            //Veri Erişim
+            Console.WriteLine(liste[1]);
+
+            foreach (var item in liste)
+            {
+                Console.WriteLine(item);
+            }
+
+            //Add Range
+            Console.WriteLine("********* Add Range ***********");
+            //string[] renkler = {"Kırmızı","Sarı","Yeşil"};
+            //liste.AddRange(renkler);
+
+            List<int> sayilar = new List<int>() { 1, 8, 3, 7, 9, 92, 5 };
+            liste.AddRange(sayilar);
+            foreach (var item in liste)
+            {
+                Console.WriteLine(item);
+            }
+
+            //Sort
+            Console.WriteLine("----- Sort ------");
+            liste.Sort();
+            foreach (var item in liste)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("------- Binary Search -------");
+            Console.WriteLine(liste.BinarySearch(9));
+
+            Console.WriteLine("------- Reverse -------");
+            liste.Reverse();
+            foreach (var item in liste)
+            {
+                Console.WriteLine(item);
+            }
+
+            liste.Clear();
+        }
+
+    }
+}
