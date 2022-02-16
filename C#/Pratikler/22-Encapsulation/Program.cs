d5f1787be71ca37f5e7e81740bc27b3a5fdf3db6
Berkcan Gümüşışık <gumusisikberkcan@gmail.com>
2022-02-16 10:30:59 +0300

    Encapsulation Added.

---
+++ C#/Pratikler/22-Encapsulation/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace _22_Encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ogrenci ogrenci = new Ogrenci();
+            ogrenci.Isim = "Berkcan";
+            ogrenci.Soyisim = "Gümüşışık";
+            ogrenci.OgrenciNo = 526;
+            ogrenci.Sinif = 3;
+            ogrenci.OgrenciBilgileriniGetir();
+
+            ogrenci.SinifAtlat();
+            ogrenci.OgrenciBilgileriniGetir();
+
+            Ogrenci ogrenci1 = new Ogrenci("Deniz","Arda",256,1);
+            ogrenci1.OgrenciBilgileriniGetir();
+
+            ogrenci1.SinifDusur();
+            ogrenci1.OgrenciBilgileriniGetir();
+        }
+    }
+    class Ogrenci
+    {
+        private string isim; //field
+
+        private string soyisim;
+        private int ogrenciNo;
+        private int sinif;
+
+        public string Isim
+        {
+            get { return isim; }
+            set { isim = value; }
+        }
+        public string Soyisim
+        {
+            get => soyisim;
+            set => soyisim = value;
+        }
+        public int OgrenciNo { get => ogrenciNo; set => ogrenciNo = value; }
+        public int Sinif
+        {
+            get => sinif;
+            set
+            {
+                if (value >= 1)
+                {
+                    Console.WriteLine("Sınıf en az 1 olabilir.");
+                    sinif = 1;
+                }
+                else
+                {
+                    sinif = value;
+                }
+            }
+        }
+
+        public Ogrenci(string ısim, string soyisim, int ogrenciNo, int sinif)
+        {
+            Isim = ısim;
+            Soyisim = soyisim;
+            OgrenciNo = ogrenciNo;
+            Sinif = sinif;
+        }
+
+        public Ogrenci()
+        {
+
+        }
+
+        public void OgrenciBilgileriniGetir()
+        {
+            Console.WriteLine("***** Öğrenci Bilgileri *****");
+            Console.WriteLine("Öğrenci Adı : {0}", this.Isim);
+            Console.WriteLine("Öğrenci Soyadı : {0}", this.Soyisim);
+            Console.WriteLine("Öğrenci Numarası : {0}", this.OgrenciNo);
+            Console.WriteLine("Öğrenci Sınıfı : {0}", this.Sinif);
+
+        }
+        public void SinifAtlat()
+        {
+            this.Sinif = this.Sinif + 1;
+        }
+        public void SinifDusur()
+        {
+            this.Sinif = this.Sinif - 1;
+        }
+    }
+
+
+}
+
+/*
+C#'ın %100 nesne yönelimli bir dil olduğundan bahsetmiştik. Encapsulation yani Kapsülleme kavramı bir özeliği başka sınıflardan saklamak yada korumaktır.
+
+*/
